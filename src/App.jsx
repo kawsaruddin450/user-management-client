adfe9c11d2f7e4c327e15235808f706ea17b1c22
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-11 13:18:48 +0600

    post data on server and live update client side

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,47 @@ import { useEffect } from 'react';
 
 function App() {
   const [users, setUsers] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:5000/users')
-    .then(res => res.json())
-    .then(data => console.log(data));
+      .then(res => res.json())
+      .then(data => setUsers(data));
   }, [])
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email};
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type' : 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('inside post method:', data);
+      const newUsers = [...users, data];
+      setUsers(newUsers);
+      form.reset();
+    });
+  }
   return (
     <>
       <h1>Users Management System</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="name" placeholder='your name' />
+        <br />
+        <input type="email" name="email" id="email" placeholder='your emai' />
+        <br />
+        <input type="submit" value="Add" />
+      </form>
+      {
+        users.map(user => <p key={user.id}>{user.id} : {user.name} : {user.email}</p>)
+      }
     </>
   )
 }
